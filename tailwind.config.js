f725f95413436389362b2e78fa9b6d6f6c7116d1
TLB30 <thelastblade30@gmail.com>
2023-08-07 21:42:00 +0330

    style left side

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
-  },
+    extend: {
+        backgroundSize: {
+            'size-200': '200% 200%',
+        },
+        backgroundPosition: {
+            'pos-0': '0% 0%',
+            'pos-100': '100% 100%',
+        },
+    },
+},
+
   plugins: [],
 }
 
