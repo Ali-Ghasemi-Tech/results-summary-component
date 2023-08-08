e684cf89e981a2cbb54e0509f356d1dec70533fc
TLB30 <thelastblade30@gmail.com>
2023-08-08 22:19:41 +0330

    make responsive for 375px mobiles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,9 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {
-        backgroundSize: {
-            'size-200': '200% 200%',
-        },
-        backgroundPosition: {
-            'pos-0': '0% 0%',
-            'pos-100': '100% 100%',
-        },
-    },
+    screens:{
+      "mobile":{"max":"375px"}
+    }
 },
 
   plugins: [],
